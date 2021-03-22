9b0c982a9347b8bbed54704079203470213d380a
yacchin1205 <yazawa@yzwlab.net>
2021-03-22 16:35:03 +0900

    Fix MOUNT_PATH environment variable

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rdmfs
+
+## How to run
+
+```
+$ docker build -t yacchin1205/rdmfs .
+$ docker run -it -p $(pwd)/mnt:/mnt -e RDM_NODE_ID=xxxxx -e RDM_TOKEN=YOUR_PERSONAL_TOKEN -e RDM_API_URL=http://192.168.168.167:8000/v2/ -e MOUNT_PATH=/mnt/test --name rdmfs --privileged yacchin1205/rdmfs
+```
--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,9 @@
 
 set -ue
 
+export RDM_MOUNT_PATH=${MOUNT_PATH:-/mnt}
+mkdir -p ${RDM_MOUNT_PATH}
+
+export DEBUG=--debug
 export OSF_TOKEN=${RDM_TOKEN}
-python3 -m rdmfs.__main__ -p ${RDM_NODE_ID} --base-url ${RDM_API_URL} --debug ${MNT_PATH:-/mnt}
+python3 -m rdmfs.__main__ -p ${RDM_NODE_ID} --base-url ${RDM_API_URL} ${DEBUG} ${RDM_MOUNT_PATH}
