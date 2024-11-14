0157d49da7378287f855493c0a116cce9996189e
Taku Miyamoto <40813148+miyamoto83@users.noreply.github.com>
2024-11-14 21:33:19 +0900

    add test

--- rdmfs/tests/test_docker.py
+++ rdmfs/tests/test_docker.py
@@ -1,4 +1,12 @@
-import pytest
+import pytest, time
+
+def wait_for_condition(condition_func, timeout=5, interval=1):
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        if condition_func():
+            return True
+        time.sleep(interval)
+    return False
 
 def test_docker(docker_container, rdm_storage):
     # List in directory /mnt/test/
@@ -7,10 +15,87 @@ def test_docker(docker_container, rdm_storage):
     output = output.decode("utf-8").strip()
     output_lines = output.splitlines()
     assert rdm_storage in output_lines
+    
+    # create and list file
+    file_name_1 = "File-1.txt"
+    exit_code, output = docker_container.exec_run(f"touch /mnt/test/{rdm_storage}/{file_name_1}")
+    assert exit_code == 0
+    def ls_file_1_func():
+        exit_code, output = docker_container.exec_run(f"ls /mnt/test/{rdm_storage}/")
+        assert exit_code == 0
+        output = output.decode("utf-8").strip()
+        output_lines = output.splitlines()
+        return file_name_1 in output_lines
+    assert wait_for_condition(ls_file_1_func)
 
-    #
-    # exit_code, output = docker_container.exec_run(f"ls /mnt/test/{storage}/")
+    # create and list file (Japanese file name)
+    file_name_2 = "日本語の含まれているファイル-2.txt"
+    exit_code, output = docker_container.exec_run(f"touch /mnt/test/{rdm_storage}/{file_name_2}")
+    assert exit_code == 0
+    def ls_file_2_func():
+        exit_code, output = docker_container.exec_run(f"ls /mnt/test/{rdm_storage}/")
+        assert exit_code == 0
+        output = output.decode("utf-8").strip()
+        output_lines = output.splitlines()
+        return file_name_2 in output_lines
+    assert wait_for_condition(ls_file_2_func)
+
+    # write text to file
+    file_content_1 = "123ABCあいう亜伊宇"
+    command = f"echo '{file_content_1}' > /mnt/test/{rdm_storage}/{file_name_1}"
+    exit_code, output = docker_container.exec_run(f"/bin/sh -c \'{command}'")
+    assert exit_code == 0
+    exit_code, output = docker_container.exec_run(f"cat /mnt/test/{rdm_storage}/{file_name_1}")
+    assert exit_code == 0
+    output = output.decode("utf-8").strip()
+    assert output == file_content_1
+
+    # make directory and file exists error
+    dir_name_1 = "Folder-1"
+    dir_name_1_2 = "Folder-1-2"
+    exit_code, output = docker_container.exec_run(f"mkdir -p /mnt/test/{rdm_storage}/{dir_name_1}/{dir_name_1_2}")
+    assert exit_code == 0
+    exit_code, output = docker_container.exec_run(f"ls /mnt/test/{rdm_storage}/{dir_name_1}/")
+    output = output.decode("utf-8").strip()
+    output_lines = output.splitlines()
+    assert dir_name_1_2 in output_lines
+    exit_code, output = docker_container.exec_run(f"mkdir /mnt/test/{rdm_storage}/{dir_name_1}/{dir_name_1_2}")
+    output = output.decode("utf-8").strip()
+    assert exit_code != 0 and "File exists" in output
+
+    # make directory (long Japanese folder name)
+    dir_name_long = "日本語で非常に長い名前のフォルダ名のフォルダ"
+    exit_code, output = docker_container.exec_run(f"mkdir /mnt/test/{rdm_storage}/{dir_name_1}/{dir_name_long}")
+    assert exit_code == 0
+    exit_code, output = docker_container.exec_run(f"ls /mnt/test/{rdm_storage}/{dir_name_1}/")
+    output = output.decode("utf-8").strip()
+    output_lines = output.splitlines()
+    assert dir_name_long in output_lines
+
+    # move file to folder
+    # path_1 = f"/mnt/test/{rdm_storage}/{file_name_1}"
+    # path_2 = f"/mnt/test/{rdm_storage}/{dir_name_1}/{dir_name_1_2}/"
+    # exit_code, output = docker_container.exec_run(f"mv {path_1} {path_2}")
     # assert exit_code == 0
+    # exit_code, output = docker_container.exec_run(f"ls /mnt/test/{rdm_storage}/")
     # output = output.decode("utf-8").strip()
     # output_lines = output.splitlines()
-    # assert "file2.txt" in output_lines
+    # assert file_name_1 not in output_lines
+    # exit_code, output = docker_container.exec_run(f"ls /mnt/test/{rdm_storage}/{dir_name_1}/{dir_name_1_2}")
+    # output = output.decode("utf-8").strip()
+    # output_lines = output.splitlines()
+    # #TODO assert
+    # #assert file_name_1 in output_lines
+    # file_name_1_renamed = "File-1-renamed.txt"
+    # path_1 = f"/mnt/test/{rdm_storage}/{dir_name_1}/{dir_name_1_2}/{file_name_1}" f"/mnt/test/{rdm_storage}/{file_name_1}"
+    # path_2 = f"/mnt/test/{rdm_storage}/{file_name_1_renamed}"
+    # exit_code, output = docker_container.exec_run(f"mv {path_1} {path_2}")
+    # assert exit_code == 0
+
+    # delete files
+    exit_code, output = docker_container.exec_run(f"rm /mnt/test/{rdm_storage}/{file_name_1}")
+    assert exit_code == 0
+    exit_code, output = docker_container.exec_run(f"rm /mnt/test/{rdm_storage}/{file_name_2}")
+    assert exit_code == 0
+    exit_code, output = docker_container.exec_run(f"rm -r /mnt/test/{rdm_storage}/{dir_name_1}")
+    assert exit_code == 0
